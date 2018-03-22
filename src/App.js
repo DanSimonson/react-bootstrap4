a2c993cf68a6c18c1b2757e28cc7e7ca669f958f
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-03-23 04:28:09 +0800

    changed to hashrouter so site works on github

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter, Route, Link, Switch} from 'react-router-dom';
 import Home from './pages/Home.js';
 import About from './pages/About.js';
-import Contact from './pages/Contact';
+import Contact from './pages/Contact.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
+      <HashRouter>
+        <Switch>
           <Route exact path="/" component={Home}/>}
           <Route path="/about" component={About}/>
           <Route path="/contact" component={Contact}/>
-        </div>
-      </Router>
+        </Switch>
+      </HashRouter>
     );
   }
 }
